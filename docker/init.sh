8c0827bb8b7ce7802cbf01974c4d29f7600bae32
Julien Armengaud <julien.armengaud81@gmail.com>
2024-03-02 16:12:19 +0100

    [UPDATE] Change output directory search name

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /core && python3 download_arome_data.py -d rain temp && sleep 65 && python3 download_arome_data.py -d clouds
-cd /core && python3 arome_data_to_image.py --mlat 41.5 --Mlat 45 --mlon -0.4 --Mlon 3.6 -b osm && cp -rf meteo_outputs/ /website/
+cd /core && python3 arome_data_to_image.py --mlat 38 --Mlat 53 --mlon -8 --Mlon 12 && cp -rf weather_outputs/ /website/
 
 cron
 httpd-foreground
--- website/script.js
+++ website/script.js
@@ -1,5 +1,5 @@
 function showSlides(n) {
-	document.getElementById("img_temp").src = "meteo_outputs/temp/image_"+n+".png";
-	document.getElementById("img_clouds").src = "meteo_outputs/clouds/image_"+n+".png";
-	document.getElementById("img_rain").src = "meteo_outputs/rain/image_"+n+".png";
+	document.getElementById("img_temp").src = "weather_outputs/temp/image_"+n+".png";
+	document.getElementById("img_clouds").src = "weather_outputs/clouds/image_"+n+".png";
+	document.getElementById("img_rain").src = "weather_outputs/rain/image_"+n+".png";
 }
