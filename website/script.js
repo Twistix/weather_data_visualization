da2644f8e0211687cc0f5404e05fc44ca66b0753
Julien Armengaud <julien.armengaud81@gmail.com>
2024-03-02 15:42:56 +0100

    [UPDATE] Add Dockerfile to create image with web serveur and program configured with cron

---
+++ website/script.js
@@ -0,0 +1,5 @@
+function showSlides(n) {
+	document.getElementById("img_temp").src = "meteo_outputs/temp/image_"+n+".png";
+	document.getElementById("img_clouds").src = "meteo_outputs/clouds/image_"+n+".png";
+	document.getElementById("img_rain").src = "meteo_outputs/rain/image_"+n+".png";
+}
