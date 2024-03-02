c6f8d7082aa3ac316f7a2a8d7526e83c01c03483
Julien Armengaud <julien.armengaud81@gmail.com>
2024-03-02 15:30:31 +0100

    [UPDATE] Restrict image generation from raw data to overlay generation using PIL

--- arome_data_to_image.py
+++ arome_data_to_image.py
@@ -7,22 +7,13 @@ from datetime import datetime, timedelta
 import math
 import eccodes
 import numpy as np
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.io.img_tiles as cimgt
+import matplotlib as mpl
+from PIL import Image
 import gc
 import json
 
 
 # ================================ Constants =====================================
-AVAILABLE_DATA_TYPES = ["rain","temp","clouds"]
-AVAILABLE_BACKGROUNDS = ["none","osm"]
-
-DEFAULT_BACKGROUND = "none"
-DEFAULT_DATA_TYPE = AVAILABLE_DATA_TYPES
-
-LAT_RATIO = 110.574
-LON_RATIO = 111.320
 
 
 # ============================= Useful functions =============================
@@ -43,13 +34,12 @@ def parse_inputs(available_data_types):
     parser.add_argument("--Mlat", type=float, help="Maximum latitude (in deg)")
     parser.add_argument("--mlon", type=float, help="Minimum longitude (in deg)")
     parser.add_argument("--Mlon", type=float, help="Maximum longitude (in deg)")
-    parser.add_argument("-b", "--background", type=str, default=DEFAULT_BACKGROUND, choices=AVAILABLE_BACKGROUNDS, help="Image background")
-    parser.add_argument('-d', "--data_types", type=str, default=DEFAULT_DATA_TYPE, nargs='+', choices=AVAILABLE_DATA_TYPES, help="Metrics wanted")
+    parser.add_argument('-d', "--data_types", type=str, default=available_data_types, nargs='+', choices=available_data_types, help="Metrics wanted")
 
     return parser.parse_args()
 
 
-def create_data_figure(min_lat, max_lat, min_lon, max_lon, data_type, current_time, modele_info):
+def create_image(min_lat, max_lat, min_lon, max_lon, data_type, current_time, data_type_info):
     #Process grib file
     print("Processing grib file for "+str(data_type)+" at hour H+"+"{:02d}".format(i))
     f = open("raw_data/"+str(data_type)+"/grib_"+str(data_type)+"_"+time_format_iso8601(current_time)+".grib2", "rb")
@@ -82,36 +72,21 @@ def create_data_figure(min_lat, max_lat, min_lon, max_lon, data_type, current_ti
     values[values>9998] = 0
 
     #Apply data_type operation
-    values = eval("values"+modele_info["data_types"][data_type]["operation"])
+    values = eval("values"+data_type_info["operation"])
 
     #Filter transparent values
-    values = np.ma.masked_array(values, eval("values"+modele_info["data_types"][data_type]["filter"]))
+    values = np.ma.masked_array(values, eval("values"+data_type_info["filter"]))
 
-    #Create figure
-    extent = [min_lon, max_lon, min_lat, max_lat]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
-    ax.set_extent(extent)
-    ax.set_axis_off()
+    #Nomalize values
+    min_val = float(data_type_info["range_min"])
+    max_val = float(data_type_info["range_max"])
+    values = (values - min_val) / (max_val - min_val)
 
-    #Plot data according to plot type
-    if (modele_info["data_types"][data_type]["plot_type"] == "heatmap"):
-        cmap = plt.get_cmap("jet")
-        plot = ax.pcolormesh(lons, lats, values, cmap=cmap, vmin=float(modele_info["data_types"][data_type]["range_min"]), vmax=float(modele_info["data_types"][data_type]["range_max"]), transform=ccrs.PlateCarree(), shading="auto", alpha=0.6)
-        fig.colorbar(plot)
+    #Create image
+    cmap = mpl.cm.get_cmap(data_type_info["cmap"])
+    im = Image.fromarray(np.uint8(cmap(values)*255))
 
-    return fig
-
-
-def add_background_to_figure(data_figure, background):
-    #Retreive axes from figure
-    ax = data_figure.get_axes()[0]
-
-    if (background == "osm"):
-        request = cimgt.OSM()
-        ax.add_image(request, 9)
-
-    return data_figure
+    return im
 
 
 if __name__ == "__main__":
@@ -135,13 +110,12 @@ if __name__ == "__main__":
     Mlat = args.Mlat
     mlon = args.mlon
     Mlon = args.Mlon
-    background = args.background
     data_types = args.data_types
 
     for data_type in data_types:
         #Initializing directory
         current_dir = os.getcwd()
-        path = current_dir+"/meteo_outputs/"+str(data_type)
+        path = current_dir+"/weather_outputs/"+str(data_type)
         if os.path.exists(path):
             shutil.rmtree(path)
         os.makedirs(path)
@@ -149,17 +123,11 @@ if __name__ == "__main__":
         current_time = datetime.now()
 
         for i in range(0, 24):
-            data_figure = create_data_figure(mlat, Mlat, mlon, Mlon, data_type, current_time, modele_info)
-            data_figure = add_background_to_figure(data_figure, background)
+            image = create_image(mlat, Mlat, mlon, Mlon, data_type, current_time, modele_info["data_types"][data_type])
 
-            #Save figure
-            data_figure.savefig("meteo_outputs/"+str(data_type)+"/image_"+str(i)+".png")
-            # Clear the current axes
-            plt.cla() 
-            # Clear the current figure
-            plt.clf() 
-            # Closes all the figure windows
-            plt.close("all")
+            #Save image
+            image.save("weather_outputs/"+str(data_type)+"/image_"+str(i)+".png")
+            
             #Collect garbage
             gc.collect()
 
