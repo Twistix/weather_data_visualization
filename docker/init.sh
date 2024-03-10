03e83b6587abcbff672717f71164fe28c581092f
Julien Armengaud <julien.armengaud81@gmail.com>
2024-03-11 00:44:37 +0100

    [DOCKER] Modify cron download job

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /core && python3 download_arome_data.py -d rain temp && sleep 65 && python3 download_arome_data.py -d clouds
+cd /core && python3 download_arome_data.py -d rain && sleep 65 && python3 download_arome_data.py -d temp && sleep 65 && python3 download_arome_data.py -d clouds
 cd /core && python3 arome_data_to_image.py --mlat 40 --Mlat 54 --mlon -8 --Mlon 12 -p EPSG:3857 && cp -rf weather_outputs/ /website/
 
 cron
